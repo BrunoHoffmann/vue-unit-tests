f74f03bddadf1d743c1a5d2e45a71da53ab5e981
Bruno Hoffmann <brunohoffmanndev@gmail.com>
2023-05-16 15:57:44 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Introducao ao vue test utils</h1>
+    <div class="component-box">
+      <search-input v-model="search"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchInput from './components/SearchInput.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchInput,
+  },
+  data() {
+    return {
+      search: ''
+    }
   }
 }
 </script>
@@ -24,5 +31,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.component-box {
+  width: 30%;
 }
 </style>
